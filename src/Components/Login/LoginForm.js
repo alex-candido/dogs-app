e0203b980fbdde1ca697ea2b44073ac663b4a82d
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-21 12:43:04 -0300

    estrutura inicial LoginForm.js

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
 
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <section>
+      <h1>Login</h1>
+      <form>
+        <Input label="Usuário" type="text" name="username"/>
+        <Input label="Senha" type="password" name="password"/>
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/criar">Cadastro</Link>
+    </section>
   )
 }
 
