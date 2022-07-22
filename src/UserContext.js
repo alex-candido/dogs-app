a99f4e022ec2141695286dcda53939d710e16598
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-22 20:28:53 -0300

    adicionado componente error, div cadastro e estilos de loginForm.js

--- src/UserContext.js
+++ src/UserContext.js
@@ -38,6 +38,7 @@ export const UserStorage = ({ children }) => {
       setLoading(true)
       const { url, options } = TOKEN_POST({ username, password });
       const tokenRes = await fetch(url, options);
+      console.log(tokenRes)
       if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
       const { token } = await tokenRes.json();
       window.localStorage.setItem('token', token);
