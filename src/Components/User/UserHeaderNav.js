2a552f844d506074678253b74f11df6d324ddc51
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-23 17:06:05 -0300

    svg componente e estilos em UserHeaderNav.js

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { UserContext } from '../../UserContext'
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
+  const [mobile, setMobile] = React.useState(null);
   const { userLogout } = React.useContext(UserContext);
 
   return (
-    <nav>
-      <NavLink to="/conta">Minhas fotos</NavLink>
-      <NavLink to="/conta/estatisticas">Estatísticas</NavLink>
-      <NavLink to="/conta/postar">Adicionar Foto</NavLink>
-      <button onClick={userLogout}>Sair</button>
+    <nav className={styles.nav}>
+      <NavLink to="/conta" end className={({isActive}) => (isActive ? styles.active : 'none')}>
+        <MinhasFotos />
+        {mobile && 'Minhas Fotos'}
+      </NavLink>
+      <NavLink to="/conta/estatisticas" className={({isActive}) => (isActive ? styles.active : 'none')}>
+        <Estatisticas />
+        {mobile && 'Estatísticas'}
+      </NavLink>
+      <NavLink to="/conta/postar" className={({isActive}) => (isActive ? styles.active : 'none')}>
+        <AdicionarFoto />
+        {mobile && 'Adicionar Foto'}
+      </NavLink>
+      <button onClick={userLogout}>
+        <Sair />
+        {mobile && 'Sair'}
+      </button>
     </nav>
   )
 }
