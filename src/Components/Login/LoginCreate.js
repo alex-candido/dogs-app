48a3696289bfb5ce64d65ad0cc528d2100b7a20e
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-22 21:54:30 -0300

    estrutura básica loginCreate.js

--- src/Components/Login/LoginCreate.js
+++ src/Components/Login/LoginCreate.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
 
 const LoginCreate = () => {
+  const username = useForm();
+  const email = useForm('email');
+  const password = useForm();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div>LoginCreate</div>
+    <section className="animeLeft">
+      <h1 className="title">Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username}/>
+        <Input label="Email" type="email" name="email" {...email}/>
+        <Input label="Senha" type="password" name="password" {...password} autoComplete="true"/>
+        <Button>Cadastrar</Button>
+      </form>
+    </section>
   )
 }
 
