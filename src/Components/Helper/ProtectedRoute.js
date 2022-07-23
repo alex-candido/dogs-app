688598bb2485d63b3a936dcd6bd8310b984f9dad
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-23 13:22:38 -0300

    adicionado ProtectedRoute.js em App.js

--- src/Components/Helper/ProtectedRoute.js
+++ src/Components/Helper/ProtectedRoute.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Navigate, Route } from 'react-router-dom';
-import { UserContext } from '../../UserContext'
+import React from 'react';
+import { UserContext } from '../../UserContext';
+import { Navigate } from 'react-router-dom';
+import User from '../User/User';
 
-const ProtectedRoute = (props) => {
-  const {login} = React.useContext(UserContext);
-  if(login === true) return <Route {...props} />
-  else if(login === false) return <Navigate to="/login"/>
+const ProtectedRoute = ({path, element}) => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <User />;
+  else if (login === false) return <Navigate to="/login" />;
   else return null;
-}
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
