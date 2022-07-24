f49ad79023f2933d2f796d19cbdf480f7d874cf6
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-24 19:49:12 -0300

    estrutura PhotoContent.js

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
+import styles from './PhotoContent.module.css';
 
 const PhotoContent = ({data}) => {
+  console.log(data)
+  const {photo, comments} = data;
   return (
-    <div>PhotoContent</div>
+    <div className={styles.photo}>
+      <div className={styles.img}>
+        <img src={photo.src} alt={photo.title}/>
+      </div>
+      <div className={styles.details}>
+        <p>
+          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          <span className={styles.visualizacoes}>{photo.acessos}</span>
+        </p>
+        <h1 className="title">
+          <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+        </h1>
+        <ul className={styles.attributes}>
+          <li>{photo.peso} kg</li>
+          <li>{photo.idade} anos</li>
+        </ul>
+      </div>
+      <PhotoComments id={photo.id} comments={comments} />
+    </div>
   )
 }
 
