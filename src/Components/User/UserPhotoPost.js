528ac4dcfcfc25a4e92bef6dfad5f4896961c77b
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-24 00:15:51 -0300

    div preview em UserPhotoPost.js

--- src/Components/User/UserPhotoPost.js
+++ src/Components/User/UserPhotoPost.js
@@ -30,6 +30,7 @@ const UserPhotoPost = () => {
 
   function handleImgChange({target}) {
     setImg({
+      preview: URL.createObjectURL(target.files[0]),
       raw: target.files[0],
     })
   }
@@ -40,9 +41,23 @@ const UserPhotoPost = () => {
         <Input label="Nome" type="text" name="nome" {...nome} />
         <Input label="Peso" type="number" name="peso" {...peso} />
         <Input label="Idade" type="number" name="idade" {...idade} />
-        <input type="file" name="img" id="img" onChange={handleImgChange}/>
+        <input
+          className={styles.file}
+          type="file"
+          name="img"
+          id="img"
+          onChange={handleImgChange}
+        />
         <Button>Enviar</Button>
       </form>
+      <div>
+        {img.preview && (
+        <div
+          className={styles.preview}
+          style={{backgroundImage: `url('${img.preview}')`}}
+        ></div>
+        )}
+      </div>
     </section>
   )
 }
