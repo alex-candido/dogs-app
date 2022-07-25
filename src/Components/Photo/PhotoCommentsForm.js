88166f23803e5301ed009eb9593f12b564e54614
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-25 15:31:25 -0300

    estrutura PhotoCommentsForm.js

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
+import { COMMENT_POST } from '../../Api';
 
 const PhotoCommentsForm = ({id}) => {
+  const [ comment, setComment ] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, {comment});
+    request(url, options)
+  }
+
   return (
-    <div>PhotoCommentsForm</div>
+    <form onSubmit={handleSubmit}>
+      <textarea 
+      id="comment"
+      name="comment"
+      placeholder="Comente..."
+      value={comment} 
+      onChange={({target}) => setComment(target.value)} 
+      />
+      <button>
+        <Enviar />
+      </button>
+    </form>
+    
   )
 }
 
