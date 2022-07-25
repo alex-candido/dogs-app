e2321850d0be1ba98ac533a734ad915272979d09
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-25 16:39:26 -0300

    map list em PhotoComments.js

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
-import { COMMENT_POST } from '../../Api';
+import Error from '../Helper/Error';
+import { COMMENT_POST } from '../../api';
 
-const PhotoCommentsForm = ({id}) => {
+const PhotoCommentsForm = ({ id,setComments }) => {
   const [ comment, setComment ] = React.useState('');
   const { request, error } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, {comment});
-    request(url, options)
+    const { response, json } = await request(url, options); 
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
   }
 
   return (
@@ -25,6 +30,7 @@ const PhotoCommentsForm = ({id}) => {
       <button>
         <Enviar />
       </button>
+        <Error error={error} />
     </form>
     
   )
