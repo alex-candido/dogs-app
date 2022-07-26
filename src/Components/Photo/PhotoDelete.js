106767cf878c376f53bfaf61db7388616d60c65f
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-25 21:51:53 -0300

    loading button em PhotoDelete.js

--- src/Components/Photo/PhotoDelete.js
+++ src/Components/Photo/PhotoDelete.js
@@ -7,15 +7,26 @@ const PhotoDelete = ({ id }) => {
   const { loading, request } = useFetch();
   
   async function handleClick(event) {
-    const { url, options } = PHOTO_DELETE(id)
-    const { response } = await request(url, options);
-    if(response.ok) window.location.reload();
+    const confirm = window.confirm('Tem certeza que deseja deletar?');
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
+    }
   }
 
   return (
-    <div>
-      <button onClick={handleClick} className={styles.delete}>Deletar</button>
-    </div>
+    <>
+      {loading ? (
+        <button className={styles.delete} disabled>
+          Deletar
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
+    </>
   )
 }
 
