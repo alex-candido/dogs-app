b0b5e322277476d0732aed3d50360b13d9d68c30
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-27 18:09:10 -0300

    estrutura inicial LoginPasswordLost.js

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import useFetch from '../../Hooks/useFetch'
+import useForm from '../../Hooks/useForm'
+import Button from '../Forms/Button'
+import Input from '../Forms/Input'
 
 const LoginPasswordLost = () => {
+  const email = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
   return (
-    <div>LoginPasswordLost</div>
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email / Usuário" type="text" name="login" {...email} />
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
   )
 }
 
--- src/api.js
+++ src/api.js
@@ -108,3 +108,4 @@ export function PHOTO_DELETE(id) {
     },
   };
 }
+
