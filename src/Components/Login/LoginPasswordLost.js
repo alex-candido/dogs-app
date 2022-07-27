df1078d9d2098139b85936f06a1cea7946d267b2
Alex Cândido <alex.candido.tec@gmail.com>
2022-07-27 18:15:26 -0300

    api PASSWORD_LOST

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -5,18 +5,20 @@ import Button from '../Forms/Button'
 import Input from '../Forms/Input'
 
 const LoginPasswordLost = () => {
-  const email = useForm();
+  const login = useForm();
   const { data, loading, error, request } = useFetch();
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    request(url, options);
   }
 
   return (
     <section>
       <h1 className="title">Perdeu a senha?</h1>
       <form onSubmit={handleSubmit}>
-        <Input label="Email / Usuário" type="text" name="login" {...email} />
+        <Input label="Email / Usuário" type="text" name="login" {...login} />
         <Button>Enviar Email</Button>
       </form>
     </section>
